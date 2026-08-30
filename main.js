c2ca5d2dc328ea57aaa83a1ea6125d2e19b34f0c
networker002 <networkuser69t@gmail.com>
2026-08-30 16:09:48 -0400

    fix

--- main.js
+++ main.js
@@ -1135,19 +1135,19 @@ function upsSV() {
       ) {
         stopAll();
         message.style.display = "block";
-        assistant.style.transform = "translate(0%, -50%)";
-        message.parentElement.style.transform = "translate(0%, -50%)";
+        //assistant.style.transform = "translate(0%, -50%)";
+        //message.parentElement.style.transform = "translate(0%, -50%)";
         message.innerHTML = `<div><div><p>Завтра тебе <span style="color:yellow;">${dayLesson?.textContent.startsWith("2") ? "ко" : "к" + dayLesson.textContent.replace("пара", "паре")}</span>!</p><p>Не пропусти ее, лучше подготовься заранее и прийди за <span style="color:rgb(0, 255, 100);">10-15</span> мин</p></div><div style="text-align:right"><button onclick="rr=false;  message.style.display = 'none';assistant.style.transform = 'translate(0)';message.parentElement.style.transform = 'translate(0)';" class="my-def-btns">Хорошо</button></div</div>`;
         clickedAi = true;
       } else if (clickedAi) {
-        assistant.style.transform = "translate(0)";
-        message.parentElement.style.transform = "translate(0)";
+        //assistant.style.transform = "translate(0)";
+        //message.parentElement.style.transform = "translate(0)";
         message.innerHTML = "";
         message.style.display = "none";
       }
     } else {
       ch = true;
-      assistant.style.transform = "translate(0)";
+      //assistant.style.transform = "translate(0)";
       //document.querySelector("header h1 span").textContent = "Мой дневник";
     }
 
